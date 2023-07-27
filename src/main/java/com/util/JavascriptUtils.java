ed390c044363bb48ac550478f365f64b080532a9
MangalaDas <loveyouchopra95@gmail.com>
2023-07-27 13:14:15 +0530

    javascriptutil class created

--- src/main/java/com/util/JavascriptUtils.java
+++ src/main/java/com/util/JavascriptUtils.java
@@ -1,2 +1,14 @@
+package com.util;
+
 //This is a dummy file added via git 
 //Javascript Utils can be added 
+
+
+
+public class JavascriptUtils{
+	
+	/*
+	 * added from eclipse branch javascriptUtil_branch
+	 */
+	
+}
